8d1573e464584cce0d6398fa796b2db39b5a484a
Seeker14 <allegoriy@list.ru>
2023-01-12 14:01:56 +0200

    4.1. & 4.2. Добавлены пробелы для классов Info, Multiple, StudentInfo

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Elena Mashkova");
         System.out.println("14.02.1988");
     }
