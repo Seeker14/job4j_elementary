ecac834e762d0ae025ba69f385efd4d1c3d2c2f4
Seeker14 <allegoriy@list.ru>
2023-01-12 12:28:57 +0200

    4.1. Разбор первой программы. Добавление класса StudentInfo с ФИО, датой рождения ученика [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args){
+        System.out.println("Elena Mashkova");
+        System.out.println("14.02.1988");
+    }
+}
