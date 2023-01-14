2ed0bc3e62ece258c7bf2eb0c920cffc5dd6a005
Seeker14 <allegoriy@list.ru>
2023-01-14 13:17:52 +0200

    7. Расстояние между точками в системе координат. Добавлены пробелы между аргументами [#188]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -13,9 +13,9 @@ public class Point {
 
     public static void main(String[] args) {
         double result = Point.distance(0, 0, 2, 0);
-        double result1 = Point.distance(1,1,9,3);
-        double result2 = Point.distance(5,1,8,5);
-        double result3 = Point.distance(1,0,2,5);
+        double result1 = Point.distance(1, 1, 9, 3);
+        double result2 = Point.distance(5, 1, 8, 5);
+        double result3 = Point.distance(1, 0, 2, 5);
         System.out.println("result (0, 0) to (2, 0) " + result);
         System.out.println("result1 (1, 1) to (9, 3) " + result1);
         System.out.println("result2 (5, 1) to (8, 5) " + result2);
