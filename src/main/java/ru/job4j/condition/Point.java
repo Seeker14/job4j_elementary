a903556e172ed26cb069504317cc005203cce95e
Seeker14 <allegoriy@list.ru>
2023-01-15 00:55:30 +0200

    7. Расстояние между точками в системе координат. Доработан метод Point, запись формулы в 1 строку [#188 #381077]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -2,13 +2,7 @@ package ru.job4j.condition;
 
 public class Point {
     public static double distance(int x1, int y1, int x2, int y2) {
-        double rsl1 = x2 - x1;
-        double rsl2 = y2 - y1;
-        rsl1 = Math.pow(rsl1, 2);
-        rsl2 = Math.pow(rsl2, 2);
-        double rsl = rsl1 +  rsl2;
-        rsl = Math.sqrt(rsl);
-        return rsl;
+        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
     }
 
     public static void main(String[] args) {
