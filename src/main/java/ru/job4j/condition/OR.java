53719bbafac2bd32f54543c4c05579585f923280
Seeker14 <allegoriy@list.ru>
2023-01-18 12:16:52 +0200

    4.3.2. Сокращенный оператор ||. Создание класса ru.job4j.condition.OR [#333784]

---
+++ src/main/java/ru/job4j/condition/OR.java
@@ -0,0 +1,10 @@
+package ru.job4j.condition;
+
+public class OR {
+    public static void main(String[] args) {
+        boolean first = false;
+        boolean second = true;
+        boolean join = first || second;
+        System.out.println(join);
+    }
+}
