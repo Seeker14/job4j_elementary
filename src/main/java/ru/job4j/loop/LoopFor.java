da376d4f9186a6b35d7967ed729518d8b45d9b2f
Seeker14 <allegoriy@list.ru>
2023-01-19 12:10:57 +0200

    5.0. Цикл for. Определение. Создание класса ru.job4j.loop.LoopFor [#3572]

---
+++ src/main/java/ru/job4j/loop/LoopFor.java
@@ -0,0 +1,12 @@
+package ru.job4j.loop;
+
+public class LoopFor {
+    public static void main(String[] args) {
+        for (int index = 0; index <= 5; index++) {
+            System.out.println(index);
+        }
+        for (int number = 5; number <= 10; number++) {
+            System.out.println("Вывод чисел от 5 до 10 :" + number);
+        }
+    }
+}
