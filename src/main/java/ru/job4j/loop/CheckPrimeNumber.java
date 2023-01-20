1d94686e2358569e097bbb4be4e2da0a4e1c94a4
Seeker14 <allegoriy@list.ru>
2023-01-20 15:55:30 +0200

    5.5. Простое число. Переопределена переменная prime + добавлены исправления [#171694 #383065]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,13 +2,11 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = true;
-        if (number == 1) {
-            return false;
-        }
+        boolean prime = number > 1;
         for (int index = 2; index < number; index++) {
               if ((number % index) == 0) {
-                return false;
+                prime = false;
+                break;
             }
         }
         return prime;
