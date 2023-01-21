f9bfe71f9b8a7d0b946c29ac6933c870b78aa737
Seeker14 <allegoriy@list.ru>
2023-01-21 07:43:21 +0200

    5.7. Ипотека. Внесение правок [#156315 #383246]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -2,10 +2,9 @@ package ru.job4j.loop;
 
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
-        int year = 1;
-        double sum = amount + amount * percent / 100;
-        while (salary < sum) {
-            sum -= salary;
+        int year = 0;
+        while (amount > 0) {
+            amount += amount * percent / 100 - salary;
             year++;
         }
         return year;
