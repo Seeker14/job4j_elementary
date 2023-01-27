02f80d83bf3b0d1fb0fa1627360b45a32cb7cb69
Seeker14 <allegoriy@list.ru>
2023-01-27 18:53:36 +0200

    6.9. String - это массив. В переменную result вынесено условие [#504884 #385453]

--- src/main/java/ru/job4j/array/JavaNameValidator.java
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -2,15 +2,13 @@ package ru.job4j.array;
 
 public class JavaNameValidator {
     public static boolean isNameValid(String name) {
-        boolean result = false;
-        if (!name.isEmpty() && isLowerLatinLetter(name.codePointAt(0))) {
+        boolean result = !name.isEmpty() && isLowerLatinLetter(name.codePointAt(0));
+        if (result) {
             for (int i = 1; i < name.length(); i++) {
                 int code = name.codePointAt(i);
                 if (!isUpperLatinLetter(code) && !isLowerLatinLetter(code) && !isSpecialSymbol(code) && !Character.isDigit(code)) {
                     result = false;
                     break;
-                } else {
-                    result = true;
                 }
             }
         }
